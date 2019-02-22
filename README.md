1bd5e3809b3d4efc1ab0e0504ad2baaa767e91eb
Alan Du <alanduu50@gmail.com>
2019-02-22 00:02:16 +0000

    Grabs random memes from /r/memes and updated README

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # meme-search
+
+`npm i -S meme-search`
+
+Using a search term/keyword, gives you an array of random memes.
+
+Currently grabs them from /r/memes subreddit (using the top results). No API key needed.
+
+# Usage
+
+```
+var memeSearch = require('meme-search')
+
+memeSearch('chicken', (err, res) => {
+    console.log(res) // Array of meme objects, each meme object of the format { title, image_url }
+})
+```
+
+
---
+++ index.js
@@ -0,0 +1,33 @@
+const request = require('request')
+
+module.exports = (keyword, ops, cb) => {
+    if (!cb) {
+        cb = ops
+        ops = {}
+    }
+
+    let uri = encodeURI('https://www.reddit.com/r/memes/search.json?q='+(keyword)+'&restrict_sr=1&sort=top')
+    
+    request({
+        method: 'GET',
+        uri
+    }, (err, response, body) => {
+        if (err) return cb(err)
+        if (response.status >= 400) return cb(new Error('Status code: ' + response.statusCode))
+
+        let data = JSON.parse(body)
+
+        let posts = data.data.children
+        let memes = []
+        
+        for (var post of posts) {
+            if (post.data.post_hint != "image") continue // Ignore posts that aren't images
+            memes.push({
+                title: post.data.title,
+                image_url: post.data.url
+            })
+        }
+        
+        cb(null, memes)
+    }).on('error', cb)
+}
